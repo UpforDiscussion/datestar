5832c5dcaa7d8eb19ccf2d5540bba1359276ef62
Avinash Karnani <avi@avikarnani.com>
2012-11-20 14:16:04 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datestar::Application.config.secret_token = '827c194ee4c823ea0f583690a24d04aa4875366d9ac2875aa9bebd91de282354e5f47418ca398ea8a42728152fb9a7891953c13d4f648fef2228160b87845c7d'
